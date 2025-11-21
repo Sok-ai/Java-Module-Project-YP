c8c127c6f39e309bb43561e3c28dd7e3c1431292
Sok-ai <Sokol152433@gmail.com>
2025-11-21 11:39:10 +0300

    move logic from Main class to Race class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,51 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        startRace();
-    }
-
-    public static void startRace() {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Car> cars = new ArrayList<>();
-        System.out.println("Добро пожаловать на гонки! Требуется ввести 3 автомобиля.");
-        int speed;
-
-        for (int i = 1; i < 4; i++) {
-            System.out.println("-----------------------");
-            System.out.println("Введите название для " + i + " автомобиля");
-            System.out.print("Название: ");
-            String name = scanner.next();
-            speed = requestSpeed();
-
-            Car car = new Car(name, speed);
-            cars.add(car);
-        }
-    }
-    private static boolean checkCorrectSpeed(int speed) {
-        return speed < 0 || speed > 250;
-    }
-
-    private static int requestSpeed() {
-        Scanner scanner = new Scanner(System.in);
-        int speed = 0;
-        System.out.println("Введите скорость");
-        System.out.print("Cкорость (целое число от 0 до 250): ");
-        try {
-            speed = scanner.nextInt();
-            while (true) {
-                if (checkCorrectSpeed(speed)) {
-                    System.out.println("Введена не верная скорость. Диапазон от 0 до 250");
-                    System.out.print("Новое значение скорости: ");
-                    speed = scanner.nextInt();
-                } else {
-                    break;
-                }
-            }
-        } catch (InputMismatchException e) {
-            System.out.println("ОШИБКА: Скорость не может быть буквой или дробью");
-            System.out.println("-----------------");
-            requestSpeed();
-        }
-        return speed;
+        Race.startRace();
     }
 }
--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -1,7 +1,57 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Race {
+    public static void startRace() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Car> cars = new ArrayList<>();
+        System.out.println("Добро пожаловать на гонки! Требуется ввести 3 автомобиля.");
+        int speed;
+
+        for (int i = 1; i < 4; i++) {
+            System.out.println("-----------------------");
+            System.out.println("Введите название для " + i + " автомобиля");
+            System.out.print("Название: ");
+            String name = scanner.next();
+            speed = requestSpeed();
+
+            Car car = new Car(name, speed);
+            cars.add(car);
+        }
+
+        countResults(cars);
+    }
+
+    private static boolean checkCorrectSpeed(int speed) {
+        return speed < 0 || speed > 250;
+    }
+
+    private static int requestSpeed() {
+        Scanner scanner = new Scanner(System.in);
+        int speed = 0;
+        System.out.println("Введите скорость");
+        System.out.print("Cкорость (целое число от 0 до 250): ");
+        try {
+            speed = scanner.nextInt();
+            while (true) {
+                if (checkCorrectSpeed(speed)) {
+                    System.out.println("Введена не верная скорость. Диапазон от 0 до 250");
+                    System.out.print("Новое значение скорости: ");
+                    speed = scanner.nextInt();
+                } else {
+                    break;
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("ОШИБКА: Скорость не может быть буквой или дробью");
+            System.out.println("-----------------");
+            requestSpeed();
+        }
+        return speed;
+    }
+
     private static void countResults(ArrayList<Car> cars) {
         for (Car car : cars) {
             int km = car.getSpeed() * 24;
