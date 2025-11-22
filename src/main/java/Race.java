799c786ad41dcac9b404db4a329a76899a9ae812
Sok-ai <Sokol152433@gmail.com>
2025-11-22 23:31:15 +0300

    optimize winner calculation and remove collections

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -10,7 +10,6 @@ public class Race {
     private static Car winner = new Car("", -1);
 
     public static void startRace() {
-        ArrayList<Car> cars = new ArrayList<>();
         System.out.println("Добро пожаловать на гонки! Требуется ввести 3 автомобиля.");
 
         for (int i = 1; i < 4; i++) {
@@ -21,7 +20,7 @@ public class Race {
             int speed = requestSpeed();
             Car car = new Car(name, speed);
 
-            cars.add(car);
+            winner = (winner.getKm() > car.getKm()) ? winner : car;
         }
 
         countResults(cars);
@@ -77,18 +76,7 @@ public class Race {
         return speed;
     }
 
-    private static void countResults(ArrayList<Car> cars) {
-        for (Car car : cars) {
-            int km = car.getSpeed() * 24;
-            car.setKM(km);
-        }
-        outputWinner(cars);
-    }
-
-    private static void outputWinner(ArrayList<Car> cars) {
-        Car winner = Collections.max(cars, (car1, car2) ->
-                Integer.compare(car1.getKM(), car2.getKM())
-        );
+    private static void outputWinner(Car car) {
         System.out.println("-------------------");
         System.out.printf("Самый лучший и быстрый автомобиль: %s", winner.getName());
     }
